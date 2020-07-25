b12ce988c69bcd2659337c6d42054a19525ccec7
15091329896 <2200364978@qq.com>
2020-07-25 20:10:53 +0800

    pringboot整合Thymeleaf实现国际化 完全跑通 参考文章  https://www.dalaoyang.cn/article/13

--- spring-boot-learn/spring-first-project/src/main/java/com/thymeleaf/controller/TestController.java
+++ spring-boot-learn/spring-first-project/src/main/java/com/thymeleaf/controller/TestController.java
@@ -1,11 +1,27 @@
 package com.thymeleaf.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Locale;
+
 @Controller
 public class TestController {
+
+    @Autowired
+    private MessageSource messageSource;
+
+    //    http://localhost:8082/
+    @RequestMapping("/")
+    public String hello(Model model){
+        Locale locale = LocaleContextHolder.getLocale();
+        model.addAttribute("message", messageSource.getMessage("message", null, locale));
+        return "IndexThymeleaf";
+    }
 
     //    http://localhost:8082/thymeleaf
     @RequestMapping("/thymeleaf")
@@ -13,5 +29,6 @@ public class TestController {
         modelMap.addAttribute("msg", "Hello , this is thymeleaf, 百里香");
         return "thymeleaf";
     }
+
 
 }
